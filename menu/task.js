5468bc21a03f00d49a7aba1d53c7401cb6779f51
YuriyGl86 <yuriygl86@gmail.com>
2023-04-19 15:38:39 +0300

    menu2

--- menu/task.js
+++ menu/task.js
@@ -1,6 +1,6 @@
 const menuLinks = document.querySelectorAll('.menu__link')
 const menuLinksArray = Array.from(menuLinks)
-let menuOpenFlag;
+let menuOpenFlags = {};  
 
 menuLinksArray.forEach(link => {
     link.addEventListener('click', linkHandler)
@@ -8,16 +8,18 @@ menuLinksArray.forEach(link => {
 
 function linkHandler(event){
     const subMenu = event.target.nextElementSibling
+    const menuFlagkey = event.target.closest('ul.menu_main').classList.value // на случай если на странице более 1 навигацинное меню
+    
     if(subMenu && subMenu.matches('ul.menu_sub')){
         event.preventDefault()
         console.log('даааа')
-        if(menuOpenFlag && menuOpenFlag != subMenu){
-            menuOpenFlag.classList.remove('menu_active')
-            menuOpenFlag = subMenu            
-        } else if (menuOpenFlag == subMenu){
-            menuOpenFlag = false
-        } else if(!menuOpenFlag){
-            menuOpenFlag = subMenu
+        if(menuOpenFlags[menuFlagkey] && menuOpenFlags[menuFlagkey] != subMenu){  //случай мы открываем другое меню
+            menuOpenFlags[menuFlagkey].classList.remove('menu_active')
+            menuOpenFlags[menuFlagkey] = subMenu            
+        } else if (menuOpenFlags[menuFlagkey] == subMenu){  //случай мы закрываем тоже самое уже открытое меню
+            menuOpenFlags[menuFlagkey] = false
+        } else if(!menuOpenFlags[menuFlagkey]){  //случай все меню закрыты
+            menuOpenFlags[menuFlagkey] = subMenu
         }
         
         subMenu.classList.toggle('menu_active')
